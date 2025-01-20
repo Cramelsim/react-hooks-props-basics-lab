245c561999973b2d2004545a0ec72f9f7b029ff2
MELODY SIMWA <simwamelody@gmail.com>
2025-01-20 15:00:28 +0300

    update

--- src/__tests__/About.test.js
+++ src/__tests__/About.test.js
@@ -1,20 +1,23 @@
-import "@testing-library/jest-dom";
+import React from "react";
 import { render, screen } from "@testing-library/react";
-
-import user from "../data/user";
 import About from "../components/About";
 
-test("renders a <p> element with the bio from props", () => {
-  render(<About bio="I made this" links={user.links} />);
-  expect(screen.queryByText("I made this")).toBeInTheDocument();
-});
+const links = {
+  github: "https://github.com/liza",
+  linkedin: "https://www.linkedin.com/in/liza/",
+};
 
+test("renders a <p> element with the bio from props", () => {
+  render(<About bio="I love to code" links={links} />);
+  const bio = screen.getByText("I love to code");
+  expect(bio).toBeInTheDocument();
+});
 test("does not render a <p> element if the bio is not included in props", () => {
-  const { container } = render(<About links={user.links} />);
+  const { container } = render(<About links={links} />);
   expect(container.querySelector("p")).toBeNull();
 });
 
 test("does not render a <p> element if the bio is an empty string", () => {
-  const { container } = render(<About bio="" links={user.links} />);
+  const { container } = render(<About bio="" links={links} />);
   expect(container.querySelector("p")).toBeNull();
 });
